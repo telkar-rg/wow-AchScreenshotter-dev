afbe5779b372dd8ee3a9db5c03493ed72d3aa0b2
telkar-rg <telkar-rg@gmx.net>
2025-02-09 07:14:06 +0100

    AchScreenshotter-3.0.3

--- AchScreenshotter/AchScreenshotter.lua
+++ AchScreenshotter/AchScreenshotter.lua
@@ -37,8 +37,10 @@ end
 -------------------------------------------------------------
 
 local AS_MOD_NAME = "Achievement Screenshotter";
-local AS_MOD_VERSION = "3.0.2";
+local AS_MOD_VERSION = "3.0.3";
 local AS_DEBUG = false;
+
+--local AS_testing = false;
 
 function AchScreens_OnLoad( )
 	if( AS_DEBUG ) then
@@ -49,6 +51,8 @@ function AchScreens_OnLoad( )
 	this:RegisterEvent( "ACHIEVEMENT_EARNED" ); -- for achievement screenshots
 	this:RegisterEvent( "PLAYER_LEVEL_UP" );    -- for leveling up screenshots
 	this:RegisterEvent( "CHAT_MSG_SYSTEM" );    -- for reputation milestone screenshots
+
+	--this:RegisterEvent( "UNIT_SPELLCAST_SUCCEEDED" ); -- for testing only
 end
 
 function AchScreens_OnEvent( self, event, ... )
@@ -64,17 +68,44 @@ function AchScreens_OnEvent( self, event, ... )
 				AS_ss_levels = true;
 				AS_ss_reps = true;
 			end
+			-- must be seperate because old users will have saved settings already
+			if( AS_hideui == nil ) then
+				AS_hideui = false;
+			end
 		end
 	elseif( event == "ACHIEVEMENT_EARNED" and AS_ss_achs ) then
 		AchScreens__wait( 2, Screenshot, ... );
     elseif( event == "PLAYER_LEVEL_UP" and AS_ss_levels ) then
-		AchScreens__wait( 1, Screenshot, ... );
+		AS_take_screenshot(1);
 	elseif( event == "CHAT_MSG_SYSTEM" and AS_ss_reps ) then
 		if( string.find(select(1,...), "You are now")
-			and string.find(select(1,...), "with") ) then
-			AchScreens__wait( 1, Screenshot, ... );
+		and string.find(select(1,...), "with") ) then
+			AS_take_screenshot(1);
 		end
+	-- elseif( event == "UNIT_SPELLCAST_SUCCEEDED" ) then -- for debugging only
+		-- print( "SPELLCAST SUCCEEDED" );
+		-- AS_take_screenshot(2);
 	end
+end
+
+function AS_take_screenshot(delay)
+	if( AS_hideui ) then
+		AchScreens__wait( delay, AS_myHide );
+		AchScreens__wait( delay, Screenshot );
+		-- UIParent:Show(); -- Doesn't work. I suspect it is related to the delay
+		-- funciton I'm using. Until I find a work around, the user will have to
+		-- press Esc after the screenshot is taken.
+	else
+		AchScreens__wait( delay, Screenshot );
+	end
+end
+
+function AS_myShow()
+	UIParent:Show();
+end
+
+function AS_myHide()
+	UIParent:Hide();
 end
 
 -----------------------------------------------------------
@@ -87,8 +118,19 @@ function SlashCmdList.SCREENSHOTTER( msg, editbox )
 		print( "AS_slash_handler()..." );
 	end
 	
-	AS_Open_Options();
-	
+	-- if( msg == "test" ) then
+		-- if( AS_testing ) then
+			-- this:UnregisterEvent( "UNIT_SPELLCAST_SUCCEEDED" );
+			-- AS_testing = false;
+			-- print( "Screenshots will not be taken on spell casts." );
+		-- else
+			-- this:RegisterEvent( "UNIT_SPELLCAST_SUCCEEDED" ); 
+			-- AS_testing = true;
+			-- print( "Screenshots will be taken on spell casts." );
+		-- end
+	-- else
+		AS_Open_Options();
+	-- end
 end
 ----------------------------------------------------------
 -- END OF SLASH COMMAND CODE
@@ -98,10 +140,11 @@ function AS_Open_Options()
 		print( "AS_Open_Options()..." );
 	end
 
-	AS_fs_title:SetText( AS_MOD_NAME .. "v." .. AS_MOD_VERSION );
+	AS_fs_title:SetText( AS_MOD_NAME .. " v." .. AS_MOD_VERSION );
 	AS_check_Achs:SetChecked(AS_ss_achs);
 	AS_check_Levels:SetChecked(AS_ss_levels);
 	AS_check_Reps:SetChecked(AS_ss_reps);
+	AS_check_hideUI:SetChecked(AS_hideui);
 	
 	AS_Options_Frame:Show();
 end
@@ -116,4 +159,9 @@ function AS_Close_Options()
 	AS_ss_achs = AS_check_Achs:GetChecked();
 	AS_ss_levels = AS_check_Levels:GetChecked();
 	AS_ss_reps = AS_check_Reps:GetChecked();
+	AS_hideui = AS_check_hideUI:GetChecked();
+	
+	if( AS_hideui ) then
+		print( "Achievement Screenshotter: press Esc to show the UI after the screenshot is taken." );
+	end
 end
