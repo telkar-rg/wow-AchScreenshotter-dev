0b95a549baacc5affe789301f1edbdfe9791c770
telkar-rg <telkar-rg@gmx.net>
2025-02-11 19:04:37 +0100

    cleanup

--- AchScreenshotter/AS_constants.lua
+++ AchScreenshotter/AS_constants.lua
@@ -5,18 +5,19 @@ AS_DEBUG = false;
 AS_MAX_NUM_ARENA_TEAMS = 3;
 AS_HORDE_ID = 0;
 AS_ALLIANCE_ID = 0;
-AS_DEFAULT_SETTINGS =	{	AS_saved_mod_version = AS_MOD_VERSION,
-							AS_hideui = false,
-							AS_ss_achs = true,
-							AS_ss_levels = true,
-							AS_ss_reps = true,
-							AS_ss_bgs = true,
-							AS_ss_bgs_wins_only = false,
-							AS_ss_arenas = false,
-							AS_ss_arenas_wins_only = false,
-							
-							AS_ss_soi_general = false,
-							AS_ss_soi_raid = false,
-							AS_ss_soi_lfg = false,
-							AS_ss_soi_arena = false,
-						};
+AS_DEFAULT_SETTINGS =	{	
+	AS_saved_mod_version = AS_MOD_VERSION,
+	AS_hideui = false,
+	AS_ss_achs = true,
+	AS_ss_levels = true,
+	AS_ss_reps = true,
+	AS_ss_bgs = true,
+	AS_ss_bgs_wins_only = false,
+	AS_ss_arenas = false,
+	AS_ss_arenas_wins_only = false,
+
+	AS_ss_soi_general = false,
+	AS_ss_soi_raid = false,
+	AS_ss_soi_lfg = false,
+	AS_ss_soi_arena = false,
+};
